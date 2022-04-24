96240f414ce4b6c70ae21dcb1f796144cedc7624
Inbar eliyahu <inbarel@post.bgu.ac.il>
2022-04-24 15:34:36 +0300

    TESTING TASK 1

---
+++ xv6-riscv/user/test.c
@@ -0,0 +1,21 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+#include "kernel/syscall.h"
+#include "kernel/memlayout.h"
+#include "kernel/riscv.h"
+
+
+int
+main(int argc, char *argv[]){
+    printf("start");
+    pause_system(10);
+    printf("done");
+   
+    
+    exit(0);
+
+}
